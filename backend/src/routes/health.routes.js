311efe81d28bf3104344eb94c5a43ba3c960ef06
kenmz0 <kennymuniz@outlook.es>
2026-02-09 19:02:20 -0500

    add method post

--- backend/src/routes/health.routes.js
+++ backend/src/routes/health.routes.js
@@ -3,11 +3,24 @@ import { pool } from '../config/bd.js';
 
 export const router = Router();
 
-router.get('/', async (req, res) =>{
-     const result = await pool.query('SELECT NOW()');
+router.get('/', async (req, res) =>{    
+    const result = await pool.query('SELECT NOW()');
     res.json({
         status: 'OK',
         message: 'Servidor funciona correctamente',
         dbTime: result.rows[0]
+    });
+});
+
+
+
+router.post('/', async (req, res) =>{    
+    const recieveData = req.body
+    const result = await pool.query('SELECT NOW()');
+    res.json({
+        status: 'OK',
+        message: 'Servidor funciona correctamente',
+        dbTime: result.rows[0],
+        req: recieveData
     });
 });
