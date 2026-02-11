b9b7ee4db7ae194d6686cb661cd037b4a9d0e00b
kenmz0 <kennymuniz@outlook.es>
2026-02-11 13:57:51 -0500

    use repository pattern in route handler

--- backend/src/repositories/organization.repository.js
+++ backend/src/repositories/organization.repository.js
@@ -1,27 +1,58 @@
 import { pool } from '../config/bd.js'
 
 export const OrganizationRepository = {
-    getAll: async () => {
-        const result = await pool.query('select * from ORGANIZATION');
-        return result.rows;
+    getAll: async (req, res) => {
+        try {
+            const result = await pool.query('select * from ORGANIZATION');
+            res.status(200).json({
+                status: 'OK',
+                message: `Query OK, ${result.length} rows returned`,
+                count: result.length,
+                data: result.rows,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Fail",
+                error: error.message
+            });
+        }
     },
-    getById: async (id) => {
-        const result = await pool.query('select * from ORGANIZATION where ID_ORG = $1', [id]);
-        return result.rows[0];
+    getById: async (req, res) => {
+        try {
+            const { id } = req.params
+            const result = await pool.query('select * from ORGANIZATION where ID_ORG = $1', [id]);
+            res.status(200).json({
+                status: 'OK',
+                message: 'Query OK, organization returned',
+                data: result.rows[0],
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Fail",
+                error: error.message
+            });
+        }
     },
-    create: async (userData) => {
-        const { name, description, email, id_owner, telephone } = userData;
-        // ? status should have a default value setting on 'active' in database
-        const status = 'active'
-        const query = 'insert into ORGANIZATION (NAME , DESCRIPTION, EMAIL , ID_OWNER , TELEPHONE , STATUS) values($1, $2, $3 ,$4, $5, $6) returning id_org';
-        const values = [name, description, email, id_owner, telephone, status]
-        const result = await pool.query(query, values);
-        res.status(201).json({
-            status: 'OK',
-            message: "organization created susccesfully",
-            id: result.rows[0]
-        });
-        return result.rows[0]
+    create: async (req, res) => {
+        try {
+            const userOrg = req.body
+            const { name, description, email, id_owner, telephone } = userOrg;
+            // ? status should have a default value setting on 'active' in database
+            const status = 'active'
+            const query = 'insert into ORGANIZATION (NAME , DESCRIPTION, EMAIL , ID_OWNER , TELEPHONE , STATUS) values($1, $2, $3 ,$4, $5, $6) returning id_org';
+            const values = [name, description, email, id_owner, telephone, status]
+            const result = await pool.query(query, values);
+            res.status(201).json({
+                status: 'OK',
+                message: "organization created susccesfully",
+                id: result.rows[0]
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "Fail",
+                error: error.message
+            });
+        }
     }
 }
 
--- backend/src/routes/organization.routes.js
+++ backend/src/routes/organization.routes.js
@@ -3,54 +3,8 @@ import { Router } from 'express';
 
 export const router = Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const result = await OrganizationRepository.getAll()
-        res.status(200).json({
-            status: 'OK',
-            message: `Query OK, ${result.length} rows returned`,
-            count: result.length,
-            data: result,
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Fail",
-            error: error.message
-        });
-    }
-});
+router.get('/', OrganizationRepository.getAll);
 
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const result = await OrganizationRepository.getById(id)
-        res.status(200).json({
-            status: 'OK',
-            message: 'Query OK, organization returned',
-            data: result,
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Fail",
-            error: error.message
-        });
-    }
-});
+router.get('/:id', OrganizationRepository.getById);
 
-
-router.post('/', async (req, res) => {
-    try {
-        const userOrg = req.body
-        const result = await OrganizationRepository.create(userOrg)
-        res.status(201).json({
-            status: 'OK',
-            message: "organization created susccesfully",
-            id: result
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Fail",
-            error: error.message
-        });
-    }
-});
+router.post('/', OrganizationRepository.create);
